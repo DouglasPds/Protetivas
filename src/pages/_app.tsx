f4b865a12828e95280ab7300141a30b2a03b19de
DouglasPds <douglas17dd@hotmail.com>
2022-09-13 19:15:31 -0300

    feat: add auth with google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import "../styles/global.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
