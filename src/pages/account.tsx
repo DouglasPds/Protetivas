ca41ee24b25701a1d445f2556e66a3334fea32ca
DouglasPds <douglas17dd@hotmail.com>
2022-09-27 14:47:41 -0300

    feat: use react-hook-form and send the data to create account

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,6 +1,34 @@
+import { useForm } from "react-hook-form";
 import Header from "../components/header";
 
 export default function Account() {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      nome: "",
+      cpf: "",
+      email: "",
+      telefone: "",
+      cep: "",
+      cidade: "",
+    },
+  });
+
+  const handleAccount = async (data, e) => {
+    e.preventDefault();
+    try {
+      await fetch("/api/account", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Header />
@@ -15,87 +43,96 @@ export default function Account() {
             Informações do Perfil
           </h1>
 
-          <div className="mt-8 md:flex items-center">
-            <div className="flex flex-col">
-              <label className="mb-3 text-sm leading-none text-gray-800">
-                Nome
-              </label>
-              <input
-                type="name"
-                tabIndex={0}
-                aria-label="nome"
-                className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              />
+          <form action="" onSubmit={handleSubmit(handleAccount)}>
+            <div className="mt-8 md:flex items-center">
+              <div className="flex flex-col">
+                <label className="mb-3 text-sm leading-none text-gray-800">
+                  Nome
+                </label>
+                <input
+                  type="text"
+                  tabIndex={0}
+                  aria-label="nome"
+                  {...register("nome")}
+                  className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                />
+              </div>
+              <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
+                <label className="mb-3 text-sm leading-none text-gray-800">
+                  CPF
+                </label>
+                <input
+                  type="number"
+                  tabIndex={0}
+                  aria-label="cpf"
+                  {...register("cpf")}
+                  className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                />
+              </div>
             </div>
-            <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
-              <label className="mb-3 text-sm leading-none text-gray-800">
-                CPF
-              </label>
-              <input
-                type="name"
-                tabIndex={0}
-                aria-label="cpf"
-                className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              />
+            <div className="mt-12 md:flex items-center">
+              <div className="flex flex-col">
+                <label className="mb-3 text-sm leading-none text-gray-800">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  tabIndex={0}
+                  aria-label="email"
+                  {...register("email")}
+                  className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                />
+              </div>
+              <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
+                <label className="mb-3 text-sm leading-none text-gray-800">
+                  Telefone
+                </label>
+                <input
+                  type="number"
+                  tabIndex={0}
+                  aria-label="telefone"
+                  {...register("telefone")}
+                  className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                />
+              </div>
             </div>
-          </div>
-          <div className="mt-12 md:flex items-center">
-            <div className="flex flex-col">
-              <label className="mb-3 text-sm leading-none text-gray-800">
-                Email
-              </label>
-              <input
-                type="email"
-                tabIndex={0}
-                aria-label="email"
-                className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              />
+            <div className="mt-12 md:flex items-center">
+              <div className="flex flex-col">
+                <label className="mb-3 text-sm leading-none text-gray-800">
+                  Cep
+                </label>
+                <input
+                  type="number"
+                  tabIndex={0}
+                  aria-label="cep"
+                  {...register("cep")}
+                  className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                />
+              </div>
+              <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
+                <label className="mb-3 text-sm leading-none text-gray-800">
+                  Cidade
+                </label>
+                <input
+                  type="text"
+                  tabIndex={0}
+                  aria-label="cidade"
+                  {...register("cidade")}
+                  className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                />
+              </div>
             </div>
-            <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
-              <label className="mb-3 text-sm leading-none text-gray-800">
-                Telefone
-              </label>
-              <input
-                type="name"
-                tabIndex={0}
-                aria-label="telefone"
-                className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              />
-            </div>
-          </div>
-          <div className="mt-12 md:flex items-center">
-            <div className="flex flex-col">
-              <label className="mb-3 text-sm leading-none text-gray-800">
-                Cep
-              </label>
-              <input
-                type="name"
-                tabIndex={0}
-                aria-label="cep"
-                className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              />
-            </div>
-            <div className="flex flex-col md:ml-12 md:mt-0 mt-8">
-              <label className="mb-3 text-sm leading-none text-gray-800">
-                Cidade
-              </label>
-              <input
-                type="name"
-                tabIndex={0}
-                aria-label="cidade"
-                className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
-              />
-            </div>
-          </div>
-          <button
-            role="button"
-            aria-label="Next step"
-            className="flex items-center justify-center py-4 px-7 focus:outline-none bg-white border rounded border-gray-400 mt-7 md:mt-14 hover:bg-gray-100  focus:ring-2 focus:ring-offset-2 focus:ring-gray-700"
-          >
-            <span className="text-sm font-medium text-center text-gray-800 capitalize">
-              Cadastrar
-            </span>
-          </button>
+            <button
+              role="button"
+              type="submit"
+              aria-label="Next step"
+              className="flex items-center justify-center py-4 px-7 focus:outline-none bg-green-500 border rounded border-gray-400 mt-7 md:mt-14 hover:bg-green-600  focus:ring-2 focus:ring-offset-2 focus:ring-gray-700"
+            >
+              <span className="text-sm font-medium text-center text-gray-800 capitalize">
+                Cadastrar
+              </span>
+            </button>
+          </form>
         </div>
       </div>
     </div>
