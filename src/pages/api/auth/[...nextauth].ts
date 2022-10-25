3af0c4d1c799b3251f5849e551826756aaac473e
DouglasPds <douglas17dd@hotmail.com>
2022-10-25 19:15:26 -0300

    refactor: use prisma adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,21 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "../../../lib/prisma";
 
-export default NextAuth({
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+  // pages: {
+  //   signIn: "/auth/signin",
+  // },
+};
