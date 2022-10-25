776a7d0bf84703864fdae5848c20b3bc4743559c
DouglasPds <douglas17dd@hotmail.com>
2022-10-25 19:51:17 -0300

    refactor: add Header component to all pages of the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { SessionProvider } from "next-auth/react";
+import Header from "../components/header";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Header />
       <Component {...pageProps} />
     </SessionProvider>
   );
